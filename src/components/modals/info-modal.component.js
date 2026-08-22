76ca0ab3260e35cd3665d1dc76eadebc6ba9bef4
ABirjandi <a.birjandi@mahware.com>
2026-08-23 02:44:39 +0330

    refactor: improve formatting of keyboard shortcut elements in InfoModalComponent

--- src/components/modals/info-modal.component.js
+++ src/components/modals/info-modal.component.js
@@ -118,7 +118,10 @@ export const InfoModalComponent = {
                             ${keyGroup
                               .map(
                                 (key) =>
-                                  `<kbd class="px-2 py-0.5 text-[10px] font-bold text-color bg-surface border border-border rounded-md shadow-2xs">${key}</kbd>`,
+                                  `<kbd
+                                    class="px-2 py-0.5 text-[10px] font-bold text-color bg-surface border border-border rounded-md shadow-2xs"
+                                    >${key}</kbd
+                                  >`,
                               )
                               .join(
                                 '<span class="text-[10px] text-muted">/</span>',
